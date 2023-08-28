24615e140ac5ff90519ee023b82b366aaab69a73
RaffDevs <rveronezdeveloper@gmail.com>
2023-08-28 19:11:13 -0300

    feats

--- JobSearch/JobSearch/Views/Login.xaml.cs
+++ JobSearch/JobSearch/Views/Login.xaml.cs
@@ -21,5 +21,10 @@ namespace JobSearch.Views
         {
             Navigation.PushAsync(new Register());
         }
+
+        private void GoHome(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new Home());
+        }
     }
 }
